8a14b1dec9204c4e25b3e5547ffa7deb2bd71053
cole <cole.brumley@gmail.com>
2016-11-16 12:20:05 -0600

    Add test templates

---
+++ tasks/exec_test.go
@@ -0,0 +1,58 @@
+package tasks
+
+import "testing"
+
+func TestExecConfig_Serialize(t *testing.T) {
+	type fields struct {
+		Command string
+		Args    []string
+		Timeout int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		e := &ExecConfig{
+			Command: tt.fields.Command,
+			Args:    tt.fields.Args,
+			Timeout: tt.fields.Timeout,
+		}
+		got, err := e.Serialize()
+		if (err != nil) != tt.wantErr {
+			t.Errorf("%q. ExecConfig.Serialize() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+			continue
+		}
+		if got != tt.want {
+			t.Errorf("%q. ExecConfig.Serialize() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func TestExec(t *testing.T) {
+	type args struct {
+		config []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		got, err := Exec(tt.args.config...)
+		if (err != nil) != tt.wantErr {
+			t.Errorf("%q. Exec() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+			continue
+		}
+		if got != tt.want {
+			t.Errorf("%q. Exec() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
